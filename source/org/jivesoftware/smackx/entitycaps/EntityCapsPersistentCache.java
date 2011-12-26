cdf7373558b624d56a4bde26a5a6a94781f7317d
Florian Schmaus <fschmaus@gmail.com>
2011-12-26 11:57:24 +0100

    Fixed simpleDirectoryPersistentCache

--- source/org/jivesoftware/smackx/entitycaps/EntityCapsPersistentCache.java
+++ source/org/jivesoftware/smackx/entitycaps/EntityCapsPersistentCache.java
@@ -5,4 +5,5 @@ import org.jivesoftware.smackx.packet.DiscoverInfo;
 public interface EntityCapsPersistentCache {
 	abstract void addDiscoverInfoByNodePersistent(String node, DiscoverInfo info);
 	abstract void replay();
+	abstract void emptyCache();
 }
--- source/org/jivesoftware/smackx/entitycaps/SimpleDirectoryPersistentCache.java
+++ source/org/jivesoftware/smackx/entitycaps/SimpleDirectoryPersistentCache.java
@@ -1,5 +1,7 @@
 package org.jivesoftware.smackx.entitycaps;
 
+
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,9 +9,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.io.StringReader;
 
 import org.jivesoftware.smack.provider.IQProvider;
 import org.jivesoftware.smack.provider.ProviderManager;
+import org.jivesoftware.smack.util.Base64;
+import org.jivesoftware.smack.util.PacketParserUtils;
 import org.jivesoftware.smackx.packet.DiscoverInfo;
 import org.xmlpull.mxp1.MXParser;
 import org.xmlpull.v1.XmlPullParser;
@@ -31,23 +36,23 @@ public class SimpleDirectoryPersistentCache implements
 
 	@Override
 	public void addDiscoverInfoByNodePersistent(String node, DiscoverInfo info) {
-		File nodeFile = new File(cacheDir, node);
-		if (nodeFile.isFile())
-			nodeFile.delete();
+	    String base64Node = Base64.encodeBytes(node.getBytes());
+		File nodeFile = new File(cacheDir, base64Node);
 		
 		try {
 			if (nodeFile.createNewFile())
 				writeInfoToFile(nodeFile, info);
 		} catch (IOException e) {
-
+		    e.printStackTrace();
 		}
 	}
 
 	@Override
 	public void replay() {
-		File[] files = cacheDir.listFiles();
+	    File[] files = cacheDir.listFiles();
 		for (File f : files) {
-			String node = f.getName();
+			String nodeBase64 = f.getName();
+			String node = new String(Base64.decode(nodeBase64));
 			DiscoverInfo info;
 			try {
 				info = restoreInfoFromFile(f);
@@ -55,18 +60,30 @@ public class SimpleDirectoryPersistentCache implements
 				e.printStackTrace();
 				continue;
 			}
+			if (info == null)
+				continue;
+			
 			EntityCapsManager.addDiscoverInfoByNode(node, info);
 		}
 	}
+	
+    public void emptyCache() {
+        File[] files = cacheDir.listFiles();
+        for (File f : files) {
+            f.delete();
+        }
+    }
 	
 	private static void writeInfoToFile(File file, DiscoverInfo info) throws IOException {
 		DataOutputStream dos = new DataOutputStream(new FileOutputStream(file));
 		dos.writeUTF(info.toXML());
 	}
 	
-	private static DiscoverInfo restoreInfoFromFile(File file) throws IOException {
-		Reader reader = new InputStreamReader(new FileInputStream(file));
-		XmlPullParser parser;
+    private static DiscoverInfo restoreInfoFromFile(File file) throws IOException {
+        DataInputStream dis = new DataInputStream(new FileInputStream(file));
+        String fileContent = dis.readUTF();
+        Reader reader = new StringReader(fileContent);
+        XmlPullParser parser;
         try {
             parser = new MXParser();
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, true);
@@ -75,17 +92,33 @@ public class SimpleDirectoryPersistentCache implements
             xppe.printStackTrace();
             return null;
         }
-        
-        DiscoverInfo iqPacket;
-        IQProvider provider = (IQProvider)ProviderManager.getInstance().getIQProvider("query","http://jabber.org/protocol/disco#info");
 
-        try {
-			iqPacket = (DiscoverInfo)provider.parseIQ(parser);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return null;
-		}
+        DiscoverInfo iqPacket;
+        IQProvider provider = (IQProvider) ProviderManager.getInstance().getIQProvider("query", "http://jabber.org/protocol/disco#info");
         
-		return iqPacket;
-	}
+        // skip the first <iq id=....> tag
+        try {
+            parser.next();
+        } catch (XmlPullParserException e1) {
+            e1.printStackTrace();
+            return null;
+        }
+
+        // Point parser to the query tag
+        try {
+            parser.next();
+        } catch (XmlPullParserException e1) {
+            e1.printStackTrace();
+            return null;
+        }
+        
+        try {
+            iqPacket = (DiscoverInfo) provider.parseIQ(parser);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return iqPacket;
+    }
 }
